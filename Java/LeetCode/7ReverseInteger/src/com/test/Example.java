312463eaec1375d483d5c0273d1b7d5aa54fc2ce
yline <957339173@qq.com>
2016-12-20 01:58:08 +0800

    this:第七题 结束 next:continue

---
+++ Java/LeetCode/7ReverseInteger/src/com/test/Example.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import junit.framework.TestCase;
+
+/**
+ * 严重怀疑正确answer
+ */
+public class Example extends TestCase
+{
+    private Solution solution;
+    
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+        solution = new Solution();
+    }
+    
+    public void testSolution()
+    {
+        assertEquals(0, solution.reverse(0));
+        assertEquals(321, solution.reverse(123));
+        assertEquals(-321, solution.reverse(-123));
+        assertEquals(0, solution.reverse(1534236469));
+        assertEquals(0, solution.reverse(-2147483648));
+    }
+    
+    @Override
+    protected void tearDown()
+        throws Exception
+    {
+        solution = null;
+        super.tearDown();
+    }
+}
---
+++ Java/LeetCode/7ReverseInteger/src/com/test/Solution.java
@@ -0,0 +1,58 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution
+{
+    public int reverse(int x)
+    {
+        if (x > Integer.MAX_VALUE || x < Integer.MIN_VALUE)
+        {
+            return 0;
+        }
+        
+        // 防止溢出
+        long result = 0;
+        
+        int signX = x > 0 ? 1 : -1;
+        int absX = x > 0 ? x : -x;
+        
+        List<Integer> numList = new ArrayList<>();
+        
+        while (absX != 0)
+        {
+            numList.add(absX % 10);
+            absX = absX / 10;
+        }
+        
+        System.out.println("list = " + numList.toString());
+        
+        for (int i = 0; i < numList.size(); i++)
+        {
+            result += numList.get(i) * Math.pow(10, numList.size() - 1 - i);
+        }
+        
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
+        {
+            return 0;
+        }
+        
+        return (int)(signX * result);
+    }
+    
+    /** 错误的 */
+    public int reverse2(int x)
+    {
+        int signX = x > 0 ? 1 : -1;
+        int absX = x > 0 ? x : -x;
+        
+        StringBuffer stringBuffer = new StringBuffer(absX + "");
+        
+        String resultStr = stringBuffer.reverse().toString();
+        
+        int result = signX * Integer.parseInt(resultStr);
+        
+        return result;
+    }
+}
