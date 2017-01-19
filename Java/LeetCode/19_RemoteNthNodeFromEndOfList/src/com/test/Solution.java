7072a190853a46acf273fe81afe82bb728a7c1a0
yline <957339173@qq.com>
2017-01-20 02:59:14 +0800

    this:19 start next:19 finish

---
+++ Java/LeetCode/19_RemoteNthNodeFromEndOfList/src/com/test/Solution.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import com.test.bean.ListNode;
+
+public class Solution
+{
+    public ListNode removeNthFromEnd(ListNode head, int n)
+    {
+        return null;
+    }
+    
+}
---
+++ Java/LeetCode/19_RemoteNthNodeFromEndOfList/src/com/test/bean/ListNode.java
@@ -0,0 +1,13 @@
+package com.test.bean;
+
+public class ListNode
+{
+    int val;
+    
+    ListNode next;
+    
+    ListNode(int x)
+    {
+        val = x;
+    }
+}
