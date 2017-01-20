c3714bbdc78a4f780913cfb100a10cefed924f25
yline <957339173@qq.com>
2017-01-20 12:06:09 +0800

    this:20 start next:20 finish

---
+++ Java/LeetCode/20ValidParentheses/src/com/test/Solution.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Solution
+{
+    /**
+     *  ()() // 1,2,3,4 【相减为奇数】
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s)
+    {
+        
+        return false;
+    }
+}
