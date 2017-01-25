50968c9285c4be26fb79d4f63aaae1778b0b78e3
yline <957339173@qq.com>
2017-01-25 13:28:56 +0800

    this:22 start next:continue

---
+++ Java/LeetCode/22GenerateParentheses/src/com/test/Example.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import junit.framework.TestCase;
+
+public class Example extends TestCase
+{
+    private Solution solution;
+    
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+        solution = new Solution();
+    }
+    
+    public void testSolution()
+    {
+        
+    }
+    
+    @Override
+    protected void tearDown()
+        throws Exception
+    {
+        solution = null;
+        super.tearDown();
+    }
+}
---
+++ Java/LeetCode/22GenerateParentheses/src/com/test/Solution.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import java.util.List;
+
+public class Solution
+{
+    public List<String> generateParenthesis(int n)
+    {
+        return null;
+    }
+}
