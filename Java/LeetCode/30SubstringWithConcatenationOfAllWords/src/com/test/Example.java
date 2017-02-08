38d17f46e93f89b279dfec06469dbcee5a354c36
yline <957339173@qq.com>
2017-02-08 18:48:44 +0800

    this:30结束 next:31开始

--- Java/LeetCode/30SubstringWithConcatenationOfAllWords/src/com/test/Example.java
+++ Java/LeetCode/30SubstringWithConcatenationOfAllWords/src/com/test/Example.java
@@ -24,7 +24,34 @@ public class Example extends TestCase
         List<Integer> result1 = new ArrayList<>();
         result1.add(0);
         result1.add(9);
+        
+        // 检测工具类
+        assertEquals(true, solution.isContain(0, s1, word1, new ArrayList<>()) != -1);
+        assertEquals(true, solution.isContain(3, s1, word1, new ArrayList<>()) != -1);
+        assertEquals(true, solution.isContain(9, s1, word1, new ArrayList<>()) != -1);
+        assertEquals(true, solution.isContain(12, s1, word1, new ArrayList<>()) != -1);
+        assertEquals(false, solution.isContain(11, s1, word1, new ArrayList<>()) != -1);
+        
+        // 检测最终结果
         assertEquals(result1.toString(), solution.findSubstring(s1, word1).toString());
+        
+        String s2 = "wordgoodgoodgoodbestword";
+        String[] word2 = new String[] {"word", "good", "best", "good"};
+        List<Integer> result2 = new ArrayList<>();
+        //result2.add(0);
+        //result2.add(4);
+        result2.add(8);
+        //result2.add(12);
+        //result2.add(16);
+        assertEquals(result2.toString(), solution.findSubstring(s2, word2).toString());
+        
+        String s3 = "barfoofoobarthefoobarman";
+        String[] word3 = new String[] {"bar", "foo", "the"};
+        List<Integer> result3 = new ArrayList<>();
+        result3.add(6);
+        result3.add(9);
+        result3.add(12);
+        assertEquals(result3.toString(), solution.findSubstring(s3, word3).toString());
     }
     
     @Override
