5d613368b35656e86b00ba677f0b2dbcabc68b72
yline <957339173@qq.com>
2018-07-06 14:30:19 +0800

    this:第33题，二分法应用

--- Java/LeetCode/33SearchInRotaedSortedArray/src/com/test/Example.java
+++ Java/LeetCode/33SearchInRotaedSortedArray/src/com/test/Example.java
@@ -16,8 +16,20 @@ public class Example extends TestCase
     
     public void testSolution()
     {
-        int number = solution.search(new int[2], 1);
-        assertEquals(number, 0);
+        int[] array = {4, 5, 6, 7, 0, 1, 2};
+        int[] array2 = {4, 5, 6, 7, 8, 1, 2, 3};
+        int[] array3 = {8, 1};
+        int[] array4 = {1, 3};
+        
+        assertEquals(4, solution.binarySearchIndex(array));
+        assertEquals(5, solution.binarySearchIndex(array2));
+        assertEquals(1, solution.binarySearchIndex(array3));
+        assertEquals(0, solution.binarySearchIndex(array4));
+        
+        assertEquals(4, solution.search(array, 0));
+        assertEquals(-1, solution.search(array, 3));
+        assertEquals(-1, solution.search(array3, 0));
+        assertEquals(0, solution.search(array4, 1));
     }
     
     @Override
--- Java/LeetCode/33SearchInRotaedSortedArray/src/com/test/Solution.java
+++ Java/LeetCode/33SearchInRotaedSortedArray/src/com/test/Solution.java
@@ -4,6 +4,85 @@ public class Solution
 {
     public int search(int[] nums, int target)
     {
+        if (nums.length == 0)
+        {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        
+        int minIndex = binarySearchIndex(nums);
+        if (target >= nums[minIndex] && target <= nums[end])
+        {
+            return bintarySearch(nums, minIndex, end, target);
+        }
+        else if (minIndex > 0 && target >= nums[start] && target <= nums[minIndex - 1])
+        {
+            return bintarySearch(nums, start, minIndex - 1, target);
+        }
+        
+        return -1;
+    }
+    
+    /**
+     * 二分法搜索，找到中间点
+     * @param array
+     * @return 中间点的值
+     */
+    public int binarySearchIndex(int[] array)
+    {
+        int start = 0, end = array.length - 1;
+        
+        int midIndex;
+        while (start < end)
+        {
+            midIndex = (end + start) >> 1;
+            if (array[midIndex] > array[midIndex + 1])
+            {
+                return (midIndex + 1);
+            }
+            
+            // 不是目标值
+            if (array[midIndex] > array[start])
+            {
+                start = midIndex;
+            }
+            else
+            {
+                end = midIndex;
+            }
+        }
         return 0;
+    }
+    
+    /**
+     * 二分法搜索
+     * @return 搜索到的值 或 -1
+     */
+    public int bintarySearch(int[] array, int start, int end, int target)
+    {
+        int min = start, max = end;
+        int mid;
+        
+        while (min <= max)
+        {
+            mid = (min + max) >> 1;
+            
+            if (target > array[mid])
+            {
+                min = mid + 1;
+            }
+            else if (target < array[mid])
+            {
+                max = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+            
+        }
+        
+        return -1;
     }
 }
