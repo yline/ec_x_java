d687699808c4bf33aabfa1baf7beef4097ac48af
yline <957339173@qq.com>
2018-07-14 14:45:54 +0800

    this:36校验数独是否合法 next:37解决数独

--- Java/LeetCode/36ValidSudoku/src/com/test/Solution.java
+++ Java/LeetCode/36ValidSudoku/src/com/test/Solution.java
@@ -1,9 +1,103 @@
 package com.test;
 
+import java.util.Arrays;
+
 public class Solution
 {
+    /**
+     * 检查数独是否合法
+     * 
+     * 注意：仅仅已填的数字是否合法，即可
+     * 
+     * 1，先检查行
+     * 2，检查列
+     * 3，检查九宫格
+     * 每次检查，赋值然后校验是否重复赋值即可
+     * 
+     * @param board
+     * @return
+     */
     public boolean isValidSudoku(char[][] board)
     {
-        return false;
+        // 9行
+        for (int i = 0; i < 9; i++)
+        {
+            if (!isValidLine(board[i]))
+            {
+                return false;
+            }
+        }
+        
+        // 9列
+        for (int i = 0; i < 9; i++)
+        {
+            if (!isValidLine(board[0][i],
+                board[1][i],
+                board[2][i],
+                board[3][i],
+                board[4][i],
+                board[5][i],
+                board[6][i],
+                board[7][i],
+                board[8][i]))
+            {
+                return false;
+            }
+        }
+        
+        // 九宫格
+        for (int i = 0; i < 9; i += 3)
+        {
+            for (int j = 0; j < 9; j += 3)
+            {
+                if (!isValidLine(board[0 + i][0 + j],
+                    board[1 + i][0 + j],
+                    board[2 + i][0 + j],
+                    board[0 + i][1 + j],
+                    board[1 + i][1 + j],
+                    board[2 + i][1 + j],
+                    board[0 + i][2 + j],
+                    board[1 + i][2 + j],
+                    board[2 + i][2 + j]))
+                {
+                    System.out.println("i = " + i + ", j = " + j);
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * 校验9个字符，是否合法
+     * @param symbol 长度为9的数组(实际不要求长度)
+     * @return
+     */
+    public boolean isValidLine(char... symbol)
+    {
+        char[] tempArray = new char[symbol.length];
+        
+        char tempChar;
+        int tempIndex;
+        for (int i = 0; i < symbol.length; i++)
+        {
+            tempChar = symbol[i];
+            tempIndex = tempChar - '1';
+            if (tempChar <= '9' && tempChar >= '1')
+            {
+                if ('\0' == tempArray[tempIndex])
+                {
+                    tempArray[tempIndex] = tempChar;
+                }
+                else
+                {
+                    System.out.println(Arrays.toString(symbol));
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
 }
---
+++ Java/LeetCode/37SudokuSolver/src/com/test/Solution.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class Solution
+{
+    public void solveSudoku(char[][] board)
+    {
+        
+    }
+}
