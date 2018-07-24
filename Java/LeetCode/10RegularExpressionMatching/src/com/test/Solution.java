2c202f3c386b9f9231d9ff2cab29b75ac2f9e303
yline <957339173@qq.com>
2018-07-25 07:46:44 +0800

    this:10考虑ing

--- Java/LeetCode/10RegularExpressionMatching/src/com/test/Solution.java
+++ Java/LeetCode/10RegularExpressionMatching/src/com/test/Solution.java
@@ -3,24 +3,42 @@ package com.test;
 import java.util.Arrays;
 
 /**
- * Implement regular expression matching with support for '.' and '*'.
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'
+ * 输入一个string和pattern，实现支持'.'和'*'的正则匹配
  * 
-'.' Matches any single character.
-'*' Matches zero or more of the preceding element.
-
-The matching should cover the entire input string (not partial).
-
-The function prototype should be:
-bool isMatch(const char *s, const char *p)
-
-Some examples:
-isMatch("aa","a") → false
-isMatch("aa","aa") → true
-isMatch("aaa","aa") → false
-isMatch("aa", "a*") → true
-isMatch("aa", ".*") → true
-isMatch("ab", ".*") → true
-isMatch("aab", "c*a*b") → true  // ? 感觉像是不匹配的,不懂
+ * '.' Matches any single character.  '.'匹配任意一个，单个字符
+ * '*' Matches zero or more of the preceding element. '*'匹配0到n个z,前一个字符
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 匹配，必须匹配整个字符串
+ * 
+ * Note:
+ *  s could be empty and contains only lowercase letters a-z.
+ *  p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ * 
+ * Example 1:
+ *  Input:  s = "aa"  p = "a"
+ *  Output: false
+ *  Explanation: "a" does not match the entire string "aa".
+ * 
+ * Example 2:
+ *  Input:  s = "aa"  p = "a*"
+ *  Output: true
+ *  Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ * 
+ * Example 3:
+ *  Input:  s = "ab"  p = ".*"
+ *  Output: true
+ *  Explanation: ".*" means "zero or more (*) of any character (.)".
+ * 
+ * Example 4:
+ *  Input:  s = "aab"  p = "c*a*b"
+ *  Output: true
+ *  Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+ * 
+ * Example 5:
+ *  Input:  s = "mississippi"  p = "mis*is*p*."
+ *  Output: false
  * 
  * @author YLine
  *
