0eb6ffbb68a3200ccf3ab2a6821f6519f70546ff
yline <957339173@qq.com>
2018-12-20 08:46:27 +0800

    this:CSS学习

--- sublime/Html/js/jquery-accordion-menu.js
+++ sublime/Html/js/jquery-accordion-menu.js
@@ -10,8 +10,8 @@
     function Plugin(element, options) {
         this.element = element;
         // this.settings = $.extend({}, defaults, options);
-        this._defaults = defaults;
-        this._name = pluginName;
+        // this._defaults = defaults;
+        // this._name = pluginName;
         this.init()
     };
     $.extend(Plugin.prototype, {
