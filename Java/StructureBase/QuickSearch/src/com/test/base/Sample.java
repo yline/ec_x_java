dd035f9889ec56af564748702e179c6a902636a7
ylineWork <linjiang@kjtpay.local>
2019-02-22 18:07:40 +0800

    this:快速查找法 --- 二分查找法 开始

---
+++ Java/StructureBase/QuickSearch/src/com/test/base/Sample.java
@@ -0,0 +1,22 @@
+package com.test.base;
+
+import junit.framework.TestCase;
+
+public class Sample extends TestCase
+{
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        // TODO Auto-generated method stub
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown()
+        throws Exception
+    {
+        // TODO Auto-generated method stub
+        super.tearDown();
+    }
+}
---
+++ Java/StructureBase/QuickSearch/src/com/test/base/Solution.java
@@ -0,0 +1,12 @@
+package com.test.base;
+
+public interface Solution
+{
+    /**
+     * .二分查找法
+     * @param array 数组
+     * @param value 给定值
+     * @return 值的位置
+     */
+    public int bintraySearch(int[] array, int value);
+}
