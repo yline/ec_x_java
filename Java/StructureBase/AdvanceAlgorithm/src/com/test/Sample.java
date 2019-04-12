7f484238b8b8bf71c72bcf556d4000cc563bbbcd
ylineWork <linjiang@kjtpay.local>
2019-04-12 21:14:29 +0800

    this:进阶，算法工程，搭建完成 next:笔记先走，工程后续补上

---
+++ Java/StructureBase/AdvanceAlgorithm/src/com/test/Sample.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * 占个位置
+ * @author YLine
+ *
+ * 2019年4月12日 下午8:50:59
+ */
+public class Sample
+{
+    
+}
---
+++ Java/StructureBase/AdvanceAlgorithm/src/com/test/greedy/ASolution.java
@@ -0,0 +1,12 @@
+package com.test.greedy;
+
+/**
+ * 留的空白
+ * @author YLine
+ *
+ * 2019年4月12日 下午9:04:20
+ */
+public class ASolution
+{
+    
+}
