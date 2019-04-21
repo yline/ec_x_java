3cd2a08301687df9a8dd8aaeae74f21f8c086a0c
yline <957339173@qq.com>
2019-04-21 21:53:07 +0800

    this：0-1背包问题

--- Java/StructureBase/AdvanceAlgorithm/src/com/test/backtracking/Knapsack.java
+++ Java/StructureBase/AdvanceAlgorithm/src/com/test/backtracking/Knapsack.java
@@ -5,7 +5,11 @@ package com.test.backtracking;
  * 有n件物品和一个容量为c的背包。第i件物品的价值是v[i]，重量是w[i]。
  * 求解将哪些物品装入背包可使价值总和最大。所谓01背包，表示每一个物品只有一个，要么装入，要么不装入。
  * 
- * 参考：https://www.cnblogs.com/lixiaolun/p/4504213.html
+ * 参考：https://www.cnblogs.com/lixiaolun/p/4504213.htmlf
+ * 
+ * 不能用贪心算法【可能出现大头的情况】
+ * 
+ * 全排列，找出最大值
  * 
  * @author YLine
  *
@@ -13,5 +17,6 @@ package com.test.backtracking;
  */
 public class Knapsack
 {
+    // 
     // todo
 }
