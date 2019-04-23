c9d9624ed771e7d81c2b98a7cdf8dd3681234812
yline <957339173@qq.com>
2019-04-23 08:17:47 +0800

    this:更新文件

---
+++ Java/StructureBase/AdvanceAlgorithm/src/com/test/dynamic/EditDistance.java
@@ -0,0 +1,18 @@
+package com.test.dynamic;
+
+/**
+ * 问题：量化两个字符的相似度，编辑距离【莱文斯距离和最长公共字符串】
+ * 
+ * 1，先回溯求解，然后，再优化为动态规划
+ * 2，有LevenDistance 和 LongestLength
+ * 
+ * 参考：https://blog.csdn.net/every__day/article/details/88226722
+ * 
+ * @author YLine
+ *
+ * 2019年4月15日 下午5:34:16
+ */
+public class EditDistance
+{
+    // todo
+}
