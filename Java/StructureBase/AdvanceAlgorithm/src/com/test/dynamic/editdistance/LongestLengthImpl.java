a5582f6419d984db987ade884e7804e7556c1179
yline <957339173@qq.com>
2019-04-23 08:17:33 +0800

    this:动态规划，构建完成

---
+++ Java/StructureBase/AdvanceAlgorithm/src/com/test/dynamic/editdistance/LongestLengthImpl.java
@@ -0,0 +1,45 @@
+package com.test.dynamic.editdistance;
+
+import com.test.dynamic.DynamicSolution;
+
+/**
+ * 问题：量化两个字符的相似度，编辑距离【莱文斯距离和最长公共字符串】
+ * 
+ * 参考：https://blog.csdn.net/every__day/article/details/88226722
+ * 
+ * @author YLine
+ *
+ * 2019年4月15日 下午5:34:16
+ */
+public class LongestLengthImpl implements DynamicSolution
+{
+    /**
+     * 回溯实现，最长公共字符串
+     */
+    public static void backtrack()
+    {
+        
+    }
+    
+    /**
+     * 动态规划实现，最长公共字符串
+     */
+    public static void dynamic()
+    {
+        
+    }
+    
+    @Override
+    public void backtrack(Object... objArray)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override
+    public void dynamic(Object... objArray)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+}
