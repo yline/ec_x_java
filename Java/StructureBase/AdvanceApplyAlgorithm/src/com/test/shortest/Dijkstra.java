81d5c0b97eb3f2ce8ecff432db5fc08c58a774df
ylineWork <linjiang@kjtpay.local>
2019-04-28 18:12:34 +0800

    this:目录建好 next:实现，（其实就是动态规划）

---
+++ Java/StructureBase/AdvanceApplyAlgorithm/src/com/test/shortest/Dijkstra.java
@@ -0,0 +1,15 @@
+package com.test.shortest;
+
+/**
+ * Dijkstra 算法 -- 单源最短路径算法（可以看做是一个动态规划）
+ * 
+ * 参考：https://time.geekbang.org/column/article/76468
+ * 
+ * @author YLine
+ *
+ * 2019年4月28日 下午5:22:11
+ */
+public class Dijkstra
+{
+    // todo
+}
---
+++ Java/StructureBase/AdvanceApplyAlgorithm/src/com/test/shortest/PathGraph.java
@@ -0,0 +1,110 @@
+package com.test.shortest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 路径图
+ * @author YLine
+ *
+ * 2019年4月28日 下午5:23:07
+ */
+public class PathGraph
+{
+    private List<Vertex> vertextList;
+    
+    public PathGraph()
+    {
+        this.vertextList = new ArrayList<>();
+    }
+    
+    public void addVertex(int id)
+    {
+        vertextList.add(new Vertex(id));
+    }
+    
+    public void addEdge(int startIndex, int endIndex, int weight)
+    {
+        checkValid(startIndex);
+        checkValid(endIndex);
+        
+        Vertex start = vertextList.get(startIndex);
+        Vertex end = vertextList.get(endIndex);
+        start.addEdge(new Edge(start, end, weight));
+    }
+    
+    private void checkValid(int index)
+    {
+        if (index < 0 || index >= vertextList.size())
+        {
+            throw new IllegalAccessError("index outOfBoundException, index = " + index);
+        }
+    }
+    
+    public int getVertexSize()
+    {
+        return vertextList.size();
+    }
+    
+    public Vertex getVertex(int index)
+    {
+        checkValid(index);
+        
+        return vertextList.get(index);
+    }
+    
+    /**
+     * 边
+     */
+    public static class Edge
+    {
+        private Vertex start; // 边起始顶点，编号
+        
+        private Vertex end; // 边终止顶点，编号
+        
+        private int weight; // 权重
+        
+        public Edge(Vertex start, Vertex end, int weight)
+        {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+        
+        public int getWeight()
+        {
+            return weight;
+        }
+    }
+    
+    /**
+     * 顶点
+     */
+    public static class Vertex
+    {
+        private int id; // 顶点编号
+        
+        private List<Edge> edgeArray;
+        
+        public Vertex(int id)
+        {
+            this.id = id;
+            this.edgeArray = new ArrayList<>();
+        }
+        
+        public void addEdge(Edge edge)
+        {
+            // 去重校验
+            for (Edge oldEdge : edgeArray)
+            {
+                if (oldEdge.end.id == edge.end.id)
+                {
+                    throw new IllegalAccessError("edge double, end id = " + edge.end.id);
+                }
+            }
+            
+            // 添加内容
+            edgeArray.add(edge);
+        }
+    }
+}
