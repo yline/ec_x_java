3fdda6e0561fe082719d609b76a7c3288fd26451
yline <957339173@qq.com>
2019-04-29 08:26:50 +0800

    this:构建了一个图 next:创建一个 “数据结构”的工程，实现一些java没有的结构

--- Java/StructureBase/AdvanceApplyAlgorithm/src/com/test/shortest/Dijkstra.java
+++ Java/StructureBase/AdvanceApplyAlgorithm/src/com/test/shortest/Dijkstra.java
@@ -11,5 +11,36 @@ package com.test.shortest;
  */
 public class Dijkstra
 {
-    // todo
+    // todo 需要实现一个小顶堆
+    public static void shortest(PathGraph graph, int start, int end)
+    {
+        
+    }
+    
+    public static PathGraph createGraph()
+    {
+        PathGraph graph = new PathGraph();
+        
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        
+        graph.addEdge(0, 1, 10);
+        graph.addEdge(0, 4, 15);
+        
+        graph.addEdge(1, 2, 15);
+        graph.addEdge(1, 3, 2);
+        
+        graph.addEdge(2, 5, 5);
+        
+        graph.addEdge(3, 2, 1);
+        graph.addEdge(3, 5, 12);
+        
+        graph.addEdge(4, 5, 10);
+        
+        return graph;
+    }
 }
