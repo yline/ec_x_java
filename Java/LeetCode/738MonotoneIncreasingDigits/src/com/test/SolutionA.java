cf560bab0ec4cce9de136859fd2c8050f28e4d2d
yline <957339173@qq.com>
2019-05-09 08:52:59 +0800

    this:738滚去上班啦啦啦

---
+++ Java/LeetCode/738MonotoneIncreasingDigits/src/com/test/SolutionA.java
@@ -0,0 +1,66 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.test.base.Solution;
+
+/**
+ * 采用空间缓存的策略
+ * @author YLine
+ *
+ * 2019年5月9日 上午8:25:07
+ */
+public class SolutionA implements Solution
+{
+    
+    @Override
+    public int monotoneIncreasingDigits(int N)
+    {
+        // N范围为 [0-10^9]
+        List<Integer> tempList = new ArrayList<>(10);
+        
+        // 存入缓存
+        int temp = N;
+        while (temp > 0)
+        {
+            tempList.add(temp % 10);
+            temp /= 10;
+        }
+        
+        // 找到首个不是单调递增的
+        int index = -1;
+        for (int i = tempList.size() - 1; i > 0; i--)
+        {
+            if (tempList.get(i) > tempList.get(i - 1))
+            {
+                index = i;
+                break;
+            }
+        }
+        
+        if (index == -1)
+        {
+            return N;
+        }
+        else
+        {
+            int result = 0;
+            for (int i = tempList.size() - 1; i > index; i--)
+            {
+                result = (result * 10 + tempList.get(i));
+            }
+            
+            // 修改的那一个
+            result = result * 10 + tempList.get(index) - 1;
+            
+            // 末尾
+            for (int i = index - 1; i >= 0; i--)
+            {
+                result = (result * 10 + 9);
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ Java/LeetCode/738MonotoneIncreasingDigits/src/com/test/base/Solution.java
@@ -0,0 +1,16 @@
+package com.test.base;
+
+/**
+ * Given a non-negative integer N, find the largest number 
+ * that is less than or equal to N with monotone increasing digits.
+ * 
+ * 给一个非负的正整数，求一个比他小或等于他，且高位到低位，单调递增的最大值
+ * 
+ * @author YLine
+ *
+ * 2019年5月9日 上午8:23:03
+ */
+public interface Solution
+{
+    public int monotoneIncreasingDigits(int N);
+}
