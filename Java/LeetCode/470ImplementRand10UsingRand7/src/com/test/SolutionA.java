2e4aebf44165c67dd3164d5b9ad27ad603159233
ylineWork <linjiang@kjtpay.local>
2019-05-27 19:14:52 +0800

    this:470 开始

---
+++ Java/LeetCode/470ImplementRand10UsingRand7/src/com/test/SolutionA.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import com.test.base.SolBase;
+
+/**
+ * Given a function rand7 which generates a uniform random integer in the range 1 to 7,
+ * write a function rand10 which generates a uniform random integer in the range 1 to 10.
+ * 
+ * 给一个可以随机生成1-7的函数，求一个随机生成1-10的函数
+ * 
+ * Do NOT use system's Math.random().
+ * 不准用系统函数
+ * 
+ * @author YLine
+ *
+ * 2019年5月27日 下午7:11:35
+ */
+public class SolutionA extends SolBase
+{
+    public int rand10()
+    {
+        return -1;
+    }
+}
---
+++ Java/LeetCode/470ImplementRand10UsingRand7/src/com/test/base/SolBase.java
@@ -0,0 +1,11 @@
+package com.test.base;
+
+import java.util.Random;
+
+public class SolBase
+{
+    public int rand7()
+    {
+        return new Random().nextInt(7) + 1;
+    }
+}
